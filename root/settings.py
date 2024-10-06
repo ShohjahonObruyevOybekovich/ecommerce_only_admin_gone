ec5148ae8c697214b9c87b9b97bec6d50a4fee67
shohjahon <shohjahonobruyev3@gmail.com>
2024-10-06 21:13:39 +0500

    all

--- root/settings.py
+++ root/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # "corsheaders",
+    "corsheaders",
     'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
