1109b3193945662cd87a08b6862c3db58ba76f1c
shohjahon <shohjahonobruyev3@gmail.com>
2024-10-06 23:41:21 +0500

    all

--- root/settings.py
+++ root/settings.py
@@ -69,6 +69,8 @@ REST_FRAMEWORK = {
     'DEFAULT_CACHE_ALIAS': 'default',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10  # Adjust the page size as needed
